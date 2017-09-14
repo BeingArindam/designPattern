b3d595e9de5830770914ce8bfaff1742ba376e12
Arindam Mondal <31777155+BeingArindam@users.noreply.github.com>
2017-09-14 15:40:49 -0700

    Updated Secure Singleton
    Protection against Cloning, Serialization added.

--- src/com/cts/design/singleton/SecureSingleton.java
+++ src/com/cts/design/singleton/SecureSingleton.java
@@ -1,12 +1,16 @@
 package com.cts.design.singleton;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
 /**
  * @author arindam
  *
  *         There is serialization.
  *
  */
-public class SecureSingleton {
+public class SecureSingleton implements Cloneable, Serializable {
+	private static final long serialVersionUID = 8839305695372212562L;
 	private static SecureSingleton singleton;
 
 	private SecureSingleton() throws InstantiationException {
@@ -15,10 +19,28 @@ public class SecureSingleton {
 		}
 	}
 
-	public static synchronized SecureSingleton getInstance() throws InstantiationException {
+	public static synchronized SecureSingleton getInstance() {
 		if (singleton == null) {
-			singleton = new SecureSingleton();
+			try {
+				singleton = new SecureSingleton();
+			} catch (InstantiationException e) {
+				e.printStackTrace();
+			}
 		}
 		return singleton;
+	}
+	
+	private void readObject(ObjectInputStream ois) throws ClassNotFoundException, IOException{
+		ois.defaultReadObject();
+		singleton = getInstance();
+	}
+	
+	private Object readResolve()  {
+	    return singleton;
+	}
+	
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		throw new CloneNotSupportedException("Clone is not supported.");
 	}
 }
